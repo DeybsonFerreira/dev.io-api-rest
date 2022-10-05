79cba974903eb4bbe2a5843c84ac1866314e9f2d
Deybson Ferreira <deybson.ferreira@iatec.com>
2022-10-04 22:00:02 -0300

    enable cors

---
+++ app.Api/Configuration/CustomCors.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace app.Api.Configuration
+{
+    public static class CustomCors
+    {
+        public static IServiceCollection AddCustomCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Development",
+                    builder =>
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+
+
+                options.AddPolicy("Production",
+                    builder =>
+                        builder
+                            .WithMethods("GET")
+                            .WithOrigins("http://desenvolvedor.io") //exemplo
+                            .SetIsOriginAllowedToAllowWildcardSubdomains()
+                            //.WithHeaders(HeaderNames.ContentType, "x-custom-header")
+                            .AllowAnyHeader());
+            });
+            return services;
+        }
+    }
+}
--- app.Api/Program.cs
+++ app.Api/Program.cs
@@ -14,12 +14,21 @@ builder.Services.AddSwaggerGen();
 builder.Services.ResolveDependency(builder.Configuration);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+builder.Services.AddCustomCors();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("Development");
+}
+else
+{
+    //alterar se for preciso para outro ambiente
+    app.UseCors("Development");
+
 }
 
 app.UseHttpsRedirection();
@@ -27,5 +36,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
